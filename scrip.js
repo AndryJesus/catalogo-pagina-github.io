f4b02e53e3a987fd9ca18eeadbb6bdeb9cd751fd
Andry Escobar <andryjescobar@gmail.com>
2025-10-25 08:16:52 -0400

    pagina catalogo

---
+++ scrip.js
@@ -0,0 +1,72 @@
+const carrito = document.getElementById('carrito');
+const elementos1 = document.getElementById('lista-1');
+const lista = document.getElementById('#lista-carrito tbody');
+const vaciarCarrito = document.getElementById('vacias-carrito');
+
+cargarEventListeners();
+
+function cargarEventListeners() {
+
+    elementos1.addEventListener('click', comprarElemento);
+    carrito.addEventListener('click', eleminarElemento);
+    vaciarCarrito.addEventListener('click', vaciarCarrito);
+
+}
+
+function comprarElemento(e) {
+    e.preventDefaut();
+    if(e.target.classList.contains('agregar-carrito')) {
+        const elemento = e.target.parentElement.parentElement;
+        leerDatosElemneto(elemento);
+    }
+}
+
+function leerDatosElemneto(elemento) {
+    const infoElemento = {
+        imagen: elemento.querySelector('img').src,
+        titulo: elemento.querySelector('h3').textContent,
+        precio: elemento.querySelector('.precio').textContent,
+        id: elemento.querySelector('a').getAttribute('data-id')
+    }
+    insertarCarrito{infoElemento};
+}
+
+function insertarCarrito(elemento) {
+
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>
+            <img src="${elemento.imagen}" width=100% >
+        </td>
+        <td>
+            ${elemento.titulo}
+        </td>
+        <td>
+            ${elemento.precio}
+        </td>
+        <td>
+            <a herf="#" class= "borrar" data-id="${elemento-id}">x </a>
+        </td>
+    
+    `;
+
+    lista.appendChild(row);
+}
+
+function eliminarElemeto(e) {
+    e.preventDefaut();
+    let elemento,
+        elementoId;
+    if(e.target.classList.contains('borrar')) {
+        e.target.parentElement.parentElement.remove()
+        elemento = e.target.parentElement.parentElement;
+        elementoId = elemento.querySelector('a').queryAttribute('data-id');
+    }
+}
+
+function vaciarCarrito() {
+    while(lista.firstChild) {
+        lista.removeChild(lista.firstChild);
+    }
+    return false;
+}
